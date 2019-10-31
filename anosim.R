ecee300a1746a214b6bee4cdd7b140b38803b38d
Jason M. Wood <sandain@hotmail.com>
2019-10-30 18:43:49 -0700

    add an anosim script

---
+++ anosim.R
@@ -0,0 +1,40 @@
+#!/usr/bin/Rscript
+
+suppressPackageStartupMessages (
+  library (vegan)
+)
+
+usage <- "Usage: <Data File> <Environment File> <Environmental Variable>"
+
+# Load the command line arguments.
+args <- commandArgs (trailingOnly = TRUE)
+
+# Verify the command line arguments.
+if (length (args) < 3) stop (usage)
+
+dataFile <- args[1]
+envFile <- args[2]
+envVar <- args[3]
+
+# Verify the data files exist.
+if (! file.exists (dataFile)) stop ("Data file not found.")
+if (! file.exists (envFile)) stop ("Environment file not found.")
+
+# Load the data files.
+data <- read.table (dataFile, header = TRUE)
+env <- read.table (envFile, header = TRUE)
+
+# Remove rows and columns with no data.
+data <- data[,intersect (rownames(env), colnames(data)),drop = FALSE]
+data <- data[rowSums (data) > 0,,drop = FALSE]
+data <- data[, colSums (data) > 0,drop = FALSE]
+env <- na.omit (env[colnames (data),])
+
+# The main data file comes in transposed from what we need.
+data <- t (data)
+
+# Run anosim.
+anosim (data, env[,envVar], distance = "bray")
+
+# Finish the script.
+q ()
