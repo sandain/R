45669b639bfad6eb2297c430cf721f3d2d34bb86
Jason M. Wood <sandain@hotmail.com>
2019-10-31 10:58:54 -0700

    anosim: plot results

--- anosim.R
+++ anosim.R
@@ -4,17 +4,18 @@ suppressPackageStartupMessages (
   library (vegan)
 )
 
-usage <- "Usage: <Data File> <Environment File> <Environmental Variable>"
+usage <- "Usage: <Data File> <Environment File> <Environmental Variable> <Output File>"
 
 # Load the command line arguments.
 args <- commandArgs (trailingOnly = TRUE)
 
 # Verify the command line arguments.
-if (length (args) < 3) stop (usage)
+if (length (args) < 4) stop (usage)
 
 dataFile <- args[1]
 envFile <- args[2]
 envVar <- args[3]
+outputFile <- args[4]
 
 # Verify the data files exist.
 if (! file.exists (dataFile)) stop ("Data file not found.")
@@ -34,7 +35,19 @@ env <- na.omit (env[colnames (data),])
 data <- t (data)
 
 # Run anosim.
-anosim (data, env[,envVar], distance = "bray")
+data.ano <- anosim (data, env[,envVar], distance = "bray")
+
+# Output the anosim summary.
+summary (data.ano)
+
+# Save the plot to a file.
+if (grepl (".png$", outputFile)) png (outputFile)
+if (grepl (".pdf$", outputFile)) pdf (outputFile)
+if (grepl (".svg$", outputFile)) svg (outputFile)
+if (grepl (".tif$", outputFile)) tiff (outputFile, compression="lzw")
+
+# Draw the anosim plot.
+plot (data.ano)
 
 # Finish the script.
 q ()
