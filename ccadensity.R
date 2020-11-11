47567364b900fe4b543a7032e86b0d8a3d799784
Jason Wood <sandain@hotmail.com>
2020-11-10 16:54:01 -0800

    Cleanup figure

--- ccadensity.R
+++ ccadensity.R
@@ -48,17 +48,37 @@ env <- env[rownames (taxa),, drop = FALSE]
 # Run CCA.
 taxa.cca <- cca (formula = formula, data = env)
 
+# Figure out the categories.
+cats <- taxa_cat[!duplicated (taxa_cat$category),]$category
+cats <- cats[order (cats)]
+
+# Calculate the height of the image.
+height <- 50 * length (cats)
+
 # Save the species plot to a file.
-if (grepl (".png$", outputFile)) png (outputFile)
-if (grepl (".pdf$", outputFile)) pdf (outputFile)
-if (grepl (".svg$", outputFile)) svg (outputFile)
+if (grepl (".png$", outputFile)) png (outputFile, width=630, height=height)
+if (grepl (".pdf$", outputFile)) pdf (outputFile, width=630, height=height)
+if (grepl (".svg$", outputFile)) svg (outputFile, width=630/72, height=height/72)
 
 # Draw the CCA Density plot.
-cats <- taxa_cat[!duplicated (taxa_cat$category),]$category
-s <- scores (taxa.cca)$species
 par (mfrow=c (length (cats), 1))
+par (mar=c (1,4.1,1,1)) # 5.1 4.1 4.1 2.1
+par (oma=c (1,0,0,0))
 for (i in 1:length (cats)) {
-  plot (density (s[rownames (taxa_cat[taxa_cat$category == cats[i],]),'CCA1']), main=cats[i], xlim=c(-2,2))
+  pts <- scores (taxa.cca)$species[rownames (taxa_cat[taxa_cat$category == cats[i],]),'CCA1', drop = FALSE]
+  if (length (pts) > 1) {
+    suppressWarnings (
+      plot (density (pts), main=cats[i], xlim=c(-2,2), ylab="Density", axes=FALSE)
+    )
+  } else {
+    plot (pts, 1.0, main=cats[i], xlim=c(-2,2), ylab="Singleton", axes=FALSE)
+  }
+  Axis (side=2, labels=TRUE)
+  if (i == length (cats)) {
+    Axis (side=1, labels=TRUE)
+  } else {
+    Axis (side=1, labels=FALSE)
+  }
 }
 
 # Finish the script.
